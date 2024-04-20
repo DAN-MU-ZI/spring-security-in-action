aa30804fc1c2dc74877bbcf4dba9ced4078f9b7f
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-04-21 05:34:54 +0900

    챕터 2-2, 2-3 코드

---
+++ ssia-ch2-ex3/src/main/java/com/project/ssiach2ex3/config/ProjectConfig.java
@@ -0,0 +1,49 @@
+package com.project.ssiach2ex3.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.httpBasic(withDefaults());
+		http.authorizeHttpRequests(auth->
+			auth.anyRequest().authenticated());
+
+		return http.build();
+	}
+
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		var userDetailsService = new InMemoryUserDetailsManager();
+
+		var user = User.withUsername("hello")
+			.password("12345")
+			.authorities("read")
+			.build();
+		userDetailsService.createUser(user);
+
+		return userDetailsService;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
